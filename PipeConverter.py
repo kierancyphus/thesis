fixed numerical integration and updated the empirical values for maximum fill time as well.

PipeConverter.py
@@ -8,9 +8,16 @@ class PipeConverter:
     def __init__(self, d_h: float = 1, c: float = 1) -> None:
         self.d_h = d_h
         self.c = c
+        self.g = 9.81
 
     def update_pressure(self, pressure: float) -> None:
         self.d_h = pressure
+
+    def calculate_c(self, diameter: Union[float, int], f: float = 0.04) -> None:
+        # Calculates the internal value of C according to the Darcy-Weisback Equation
+        # The pipes are assumed to be smooth and a friction factor of 0.04 is applied
+        # TODO: change this to provide different friction factors based on pipes
+        self.c = f / diameter / 2 / self.g
 
     def fill_numerically(self, t: np.ndarray, theta: float) -> Tuple[np.ndarray, np.ndarray]:
         def derivative(x, t, delta_h, c, theta):
@@ -20,11 +27,11 @@ class PipeConverter:
         x: np.ndarray = odeint(derivative, 1e-7, t, args=(self.d_h, self.c, theta))
         return t, x
 
-    def equivalent_length(self, length: Union[int, float], d_z: Union[int, float]) -> float:
+    def equivalent_length(self, length: Union[int, float], d_z: Union[int, float], diameter: Union[int, float]) -> float:
         """
         Note: This assumes that the pipe fills in less than three minutes.
 
-        :param max_simulation_time: how long the simulation should run for (longer pipes take longer to fill)
+        :param diameter: diameter of pipe used in calculating friction coefficient
         :param length: total pipe length
         :param d_z: the height difference of the pipe. Can be negative (but not greater than the assumed d_h)
         :return:
@@ -33,9 +40,12 @@ class PipeConverter:
         # if it's pretty much flat just use flat pipe
         if np.abs(d_z) < 0.01:
             return 0.44 * length
-
+        # update the friction factor
+        self.calculate_c(diameter)
+        print(self.c)
         # estimate simulation time needed
         max_simulation_time = SimulationTimeGuesser(length, d_z, 100, type="poly_length_offset").evaluate()
+        print(f"max_simulation_time: {max_simulation_time}, * c: {max_simulation_time * self.c}")
         # numerically fill pipe
         t = np.linspace(0, max_simulation_time, 10000)
         theta = np.arcsin(d_z / length)

pipe_length_to_fill_time_ratio.py
@@ -6,7 +6,7 @@ from SimulationTimeGuesser import SimulationTimeGuesser
 
 
 class PipeConverterExperimenter:
-    def __init__(self, min_length: int = 10, max_length: int = 1000, min_height: int = 1, max_height: int = 10) -> None:
+    def __init__(self, min_length: int = 10, max_length: int = 1000, min_height: int = 1, max_height: int = 10, diameter: float = 0.3) -> None:
         """
         Class to run experiments
 
@@ -18,7 +18,9 @@ class PipeConverterExperimenter:
 
         self.min_length, self.max_length = min_length, max_length
         self.min_height, self.max_height = min_height, max_height
+        self.diameter = diameter
         self.converter = PipeConverter()
+        self.converter.calculate_c(diameter)
 
     def get_parameters(self) -> Tuple[np.ndarray, np.ndarray]:
         lengths = np.linspace(self.min_length, self.max_length, 20)
@@ -73,7 +75,7 @@ class PipeConverterExperimenter:
             self.converter.update_pressure(2 * height)
             fill_times = []
             for length in lengths:
-                time = SimulationTimeGuesser(length, height, offset, type).evaluate()
+                time = SimulationTimeGuesser(length, height, self.converter.c, offset, type).evaluate()
                 # time = length
 
                 t = np.linspace(0, time, num_points)
@@ -89,7 +91,7 @@ class PipeConverterExperimenter:
                 plt.plot(lengths, fill_times, label=f"height: {height}")
 
         if plot:
-            plt.plot(lengths, [SimulationTimeGuesser(length, 0, offset, type).evaluate() for length in lengths], label="max time", color="black")
+            plt.plot(lengths, [SimulationTimeGuesser(length, 0, self.converter.c, offset, type).evaluate() for length in lengths], label="max time", color="black")
             plt.title('Variable time integration window; max_time: length')
             plt.xlabel("pipe length")
             plt.ylabel("Time (s)")
@@ -99,8 +101,8 @@ class PipeConverterExperimenter:
 
 
 if __name__ == "__main__":
-    experimenter = PipeConverterExperimenter(10, 75000, 1, 10)
-    experimenter.constant_time()
+    experimenter = PipeConverterExperimenter(10, 50000, 1, 10)
+    # experimenter.constant_time()
     experimenter.variable_time(type="poly_length_offset")
 
     results = """